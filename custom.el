ba004426008c54ba5ae0af091f236aace3d8d434
Lauren Arnett <lba2138@columbia.edu>
2019-07-08 23:56:20 -0400

    small update

--- custom.el
+++ custom.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(evil-collection-setup-minibuffer t))
+ '(evil-collection-setup-minibuffer t)
+ '(send-mail-function (quote smtpmail-send-it))
+ '(smtpmail-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-service 25))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
